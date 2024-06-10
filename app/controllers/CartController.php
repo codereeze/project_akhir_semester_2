867a95d02698fcc6316f2c2dcc6e8e631701ae74
neibreen <azfaalharits25@gmail.com>
2024-06-10 20:39:49 +0700

    save

--- app/controllers/CartController.php
+++ app/controllers/CartController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controllers;
 
+use App\Models\Cart;
 use Framework\Controller;
 
 class CartController extends Controller
 {
     public function cart()
     {
+        $cart = new Cart();
         return $this->render('users/cart', [
-            'title' => 'Keranjang Saya'
+            'title' => 'Keranjang Saya',
+            'carts' => $cart->findAllById('user_id', $_SESSION['id'])
         ]);
     }
 }
--- resources/views/users/cart.php
+++ resources/views/users/cart.php
@@ -2,6 +2,7 @@
     <div class="pt-36">
         <h1 class="font-extrabold text-3xl mb-2"><i class="fas fa-shopping-cart"></i> Keranjang</h1>
         <hr class="mb-5">
+        <?php foreach($params['carts'] as $item): ?>
         <div class="flex items-center max-w-full w-full mb-4">
             <img src="https://www.static-src.com/wcsstore/Indraprastha/images/catalog/medium/MTA-58296597/9_to_12_9_to_12_signature_overlap_semi_blazer_shirt_-_ballet_pink_full02_dl6mail5.jpeg?w=276" class="rounded-md" width="130" alt="">
             <div class="text-xs ml-5 mr-16 self-center">
@@ -16,6 +17,7 @@
             </button>
             <button class="p-2 rounded-md border text-red-primary border-red-primary duration-300 hover:bg-red-primary hover:text-white font-bold text-base"><i class="fas fa-trash"></i> Hapus</button>
         </div>
+        <?php endforeach; ?>
         <div class="flex items-center max-w-full w-full mb-4">
             <img src="https://www.static-src.com/wcsstore/Indraprastha/images/catalog/medium/MTA-58296597/9_to_12_9_to_12_signature_overlap_semi_blazer_shirt_-_ballet_pink_full02_dl6mail5.jpeg?w=276" class="rounded-md" width="130" alt="">
             <div class="text-xs ml-5 mr-16 self-center">
